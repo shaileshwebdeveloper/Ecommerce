f9ad33f48dab9a951b479cfb6fe944660aee591b
shaileshwebdeveloper <shailesh.webdevelopment@gmail.com>
2023-01-05 21:55:08 +0530

    pagination done

--- src/Component/Pagination.jsx
+++ src/Component/Pagination.jsx
@@ -1,17 +1,24 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button } from "@chakra-ui/react";
 
-export const Pagination = ({page}) => {
-
-
-  return (
-    <div>
-
-        <Button disabled={page===1}>Prev</Button>
-                 {page}
-        <Button>Next</Button>
+  
+  
+export const Pagination = ({page, skip, changeSkip }) => {
 
 
-    </div>
-  )
-}
+  
+    return (
+    <Box>
+        
+        <Button onClick={() => changeSkip (page-1, skip-12)} bg="black" color={'white'}
+        disabled={page === 1}>Prev</Button>
+        <Button bg="black" color={'white'} >{page}</Button>
+        <Button bg="black" color={'white'} onClick={() => changeSkip(page+1, skip+12)}>Next</Button>
+
+          
+    </Box>
+   
+    );
+  }
+  
+  export default Pagination;
+  
